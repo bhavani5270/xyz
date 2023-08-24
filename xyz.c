856e4f64cd1a88357d8fd985f3341cb3b56cd5a9
bhavani5270 <141107190+bhavani5270@users.noreply.github.com>
2023-08-24 09:15:51 +0530

    Add files via upload

---
+++ xyz.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+void main(){
+int a=3;
+int b;
+int *p=&a;
+int *q=p;
+int *r=&b;
+printf("Enter the value::");
+Scanf("%d",&r);
+printf("%d %d\n",a,b);
+printf("%d %d %d \n",*p,*q,*r);
+q=&b;
+
+if(p==q)
+printf("1\n");
+
+if(q==r)
+printf("2\n");
+if(*p==*q)
+printf("3\n");
+if(*q==*r)
+printf("4\n");
+if(*p==*r)
+printf("5\n");}
